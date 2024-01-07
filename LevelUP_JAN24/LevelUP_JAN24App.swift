ae09b5ab7cb2bcc29f8165a2d5f5188afccb6a3a
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-01-07 09:51:59 +0530

    Initial Commit

---
+++ LevelUP_JAN24/LevelUP_JAN24App.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUP_JAN24App.swift
+//  LevelUP_JAN24
+//
+//  Created by Joaquim Menezes on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LevelUP_JAN24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
